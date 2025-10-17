3a6b7665ea64d79998d5220a9378a07806edce84
Christian Brosius <Christian.Brosius@gmail.com>
2025-10-17 19:41:45 +0200

    add more hardware-stats

--- main/web_api.c
+++ main/web_api.c
@@ -5,6 +5,7 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_netif.h"
+#include "esp_partition.h"
 #include "cJSON.h"
 #include "nvs_flash.h"
 #include "nvs.h"
@@ -1165,7 +1166,141 @@ static esp_err_t get_system_info_handler(httpd_req_t *req)
     cJSON_AddNumberToObject(root, "chip_revision", 0);
     cJSON_AddNumberToObject(root, "cpu_cores", 2);
     cJSON_AddNumberToObject(root, "cpu_freq_mhz", 240);
-    cJSON_AddNumberToObject(root, "flash_size_mb", 8);
+    
+    // Get actual flash size - will be calculated from partitions
+    uint32_t flash_size = 0;
+    
+    // Flash partition information - create array of partitions
+    cJSON *partitions_array = cJSON_CreateArray();
+    size_t total_used = 0;
+    
+    esp_partition_iterator_t it = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
+    while (it != NULL) {
+        const esp_partition_t *part = esp_partition_get(it);
+        
+        // Create partition object
+        cJSON *partition = cJSON_CreateObject();
+        cJSON_AddStringToObject(partition, "label", part->label);
+        
+        // Add partition type as string
+        const char *type_str = "unknown";
+        if (part->type == ESP_PARTITION_TYPE_APP) {
+            type_str = "app";
+        } else if (part->type == ESP_PARTITION_TYPE_DATA) {
+            type_str = "data";
+        }
+        cJSON_AddStringToObject(partition, "type", type_str);
+        
+        // Add subtype as string
+        char subtype_str[32];
+        if (part->type == ESP_PARTITION_TYPE_APP) {
+            if (part->subtype == ESP_PARTITION_SUBTYPE_APP_FACTORY) {
+                strcpy(subtype_str, "factory");
+            } else if (part->subtype == ESP_PARTITION_SUBTYPE_APP_OTA_0) {
+                strcpy(subtype_str, "ota_0");
+            } else if (part->subtype == ESP_PARTITION_SUBTYPE_APP_OTA_1) {
+                strcpy(subtype_str, "ota_1");
+            } else {
+                snprintf(subtype_str, sizeof(subtype_str), "0x%02x", part->subtype);
+            }
+        } else if (part->type == ESP_PARTITION_TYPE_DATA) {
+            if (part->subtype == ESP_PARTITION_SUBTYPE_DATA_NVS) {
+                strcpy(subtype_str, "nvs");
+            } else if (part->subtype == ESP_PARTITION_SUBTYPE_DATA_PHY) {
+                strcpy(subtype_str, "phy");
+            } else if (part->subtype == ESP_PARTITION_SUBTYPE_DATA_FAT) {
+                strcpy(subtype_str, "fat");
+            } else if (part->subtype == ESP_PARTITION_SUBTYPE_DATA_SPIFFS) {
+                strcpy(subtype_str, "spiffs");
+            } else {
+                snprintf(subtype_str, sizeof(subtype_str), "0x%02x", part->subtype);
+            }
+        } else {
+            snprintf(subtype_str, sizeof(subtype_str), "0x%02x", part->subtype);
+        }
+        cJSON_AddStringToObject(partition, "subtype", subtype_str);
+        
+        cJSON_AddNumberToObject(partition, "address", part->address);
+        cJSON_AddNumberToObject(partition, "size", part->size);
+        
+        // Try to get used space for NVS partitions
+        int32_t used_bytes = -1; // -1 means unknown
+        if (part->type == ESP_PARTITION_TYPE_DATA && part->subtype == ESP_PARTITION_SUBTYPE_DATA_NVS) {
+            // For NVS partitions, try to get statistics
+            nvs_stats_t nvs_stats;
+            nvs_handle_t nvs_handle;
+            
+            // Try to open the NVS partition
+            esp_err_t err = nvs_open_from_partition(part->label, "storage", NVS_READONLY, &nvs_handle);
+            if (err == ESP_OK) {
+                nvs_close(nvs_handle);
+                
+                // Get NVS statistics
+                err = nvs_get_stats(part->label, &nvs_stats);
+                if (err == ESP_OK) {
+                    // Calculate used space: (total - free) entries
+                    size_t used_entries = nvs_stats.used_entries;
+                    size_t total_entries = nvs_stats.total_entries;
+                    
+                    // Estimate used bytes (rough approximation)
+                    // Each entry uses approximately 32 bytes on average
+                    used_bytes = (int32_t)(used_entries * 32);
+                    
+                    // Add NVS statistics
+                    cJSON_AddNumberToObject(partition, "nvs_used_entries", used_entries);
+                    cJSON_AddNumberToObject(partition, "nvs_free_entries", nvs_stats.free_entries);
+                    cJSON_AddNumberToObject(partition, "nvs_total_entries", total_entries);
+                }
+            }
+        }
+        
+        cJSON_AddNumberToObject(partition, "used_bytes", used_bytes);
+        
+        cJSON_AddItemToArray(partitions_array, partition);
+        total_used += part->size;
+        
+        it = esp_partition_next(it);
+    }
+    esp_partition_iterator_release(it);
+    
+    cJSON_AddItemToObject(root, "partitions", partitions_array);
+    
+    // Determine actual flash size based on partition table
+    // Round up to nearest standard flash size (2, 4, 8, 16, 32 MB)
+    size_t max_address = 0;
+    esp_partition_iterator_t it2 = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
+    while (it2 != NULL) {
+        const esp_partition_t *part = esp_partition_get(it2);
+        size_t part_end = part->address + part->size;
+        if (part_end > max_address) {
+            max_address = part_end;
+        }
+        it2 = esp_partition_next(it2);
+    }
+    esp_partition_iterator_release(it2);
+    
+    // Determine flash size from max partition address
+    if (max_address <= 2 * 1024 * 1024) {
+        flash_size = 2 * 1024 * 1024;
+    } else if (max_address <= 4 * 1024 * 1024) {
+        flash_size = 4 * 1024 * 1024;
+    } else if (max_address <= 8 * 1024 * 1024) {
+        flash_size = 8 * 1024 * 1024;
+    } else if (max_address <= 16 * 1024 * 1024) {
+        flash_size = 16 * 1024 * 1024;
+    } else {
+        flash_size = 32 * 1024 * 1024;
+    }
+    
+    uint32_t flash_size_mb = flash_size / (1024 * 1024);
+    cJSON_AddNumberToObject(root, "flash_size_mb", flash_size_mb);
+    
+    // Calculate available flash (total - used)
+    size_t flash_available = flash_size > total_used ? flash_size - total_used : 0;
+    
+    cJSON_AddNumberToObject(root, "flash_used_bytes", total_used);
+    cJSON_AddNumberToObject(root, "flash_available_bytes", flash_available);
+    cJSON_AddNumberToObject(root, "flash_total_bytes", flash_size);
     
     // MAC address
     uint8_t mac[6] = {0};
